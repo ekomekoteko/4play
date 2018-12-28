5a966eb9c23082a74ec3cb1947f8b8f791e2791e
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-28 07:21:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -318,17 +318,7 @@ function play(guild, song) {
 }
 });
 
-client.on('message', msg => {
 
-    if (msg.content == '4join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
 client.on('ready', () => {
     client.channels.get("526434295099686924").join(); 
     });
